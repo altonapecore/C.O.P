172ee910b102b3f04dd4f2c465ff0e6bcf09f907
Trenton <tplager1@gmail.com>
2018-02-21 15:43:35 -0500

    Started creating some parent classes for later use

---
+++ Purpose/GameObject.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Purpose
+{
+    class GameObject
+    {
+        //fields
+        protected Rectangle position;
+        protected Texture2D texture;
+
+        //properties
+        public Rectangle Position { get { return position; } }
+        public int X { get { return position.X; } }
+        public int Y { get { return position.Y; } }
+        public Texture2D Texture { get { return texture; } }
+
+        //constructor
+        public GameObject(Rectangle position, Texture2D texture)
+        {
+            this.position = position;
+            this.texture = texture;
+        }
+
+        //methods
+        
+    }
+}
---
+++ Purpose/Item.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Purpose
+{
+    class Item : GameObject
+    {
+        //fields
+        //bool active;
+
+        //properties
+        //public bool Active
+        //{
+        //    get { return active; }
+        //    set { active = value; }
+        //}
+        
+        //constructor
+        public Item(Rectangle position, Texture2D texture) : base(position, texture)
+        {
+
+        }
+
+        //methods
+    }
+}
