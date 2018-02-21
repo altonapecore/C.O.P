8fa21fae9c84a3f71d7d690d661b4c2bfca017b1
vp2350 <vp2350@IGMOH1380-06>
2018-02-21 09:57:14 -0500

    Comitted

--- Purpose/Game1.cs
+++ Purpose/Game1.cs
@@ -5,7 +5,7 @@ using Microsoft.Xna.Framework.Input;
 namespace Purpose
 {
     /// <summary>
-    /// This is the main type for your game.
+    /// Vansh is here
     /// </summary>
     public class Game1 : Game
     {
