a4b24397b5910e5331a657356f242ed782dd6174
Trenton <tplager1@gmail.com>
2018-03-30 17:17:01 -0400

    UI Changes

--- Purpose/Game1.cs
+++ Purpose/Game1.cs
@@ -38,6 +38,7 @@ namespace Purpose
         Game,
         Pause,
         UpgradeMenu,
+        NextWave,
         GameOver
     }
     public class Game1 : Game
@@ -69,7 +70,7 @@ namespace Purpose
         private Texture2D roundedFrame;
 
         private Texture2D upgradeScreen;
-        private GameObject returnToPauseButton;
+        private GameObject returnToNewWaveButton;
         private GameObject groundPoundButton;
         private GameObject attackUpButton;
         private GameObject staminaUpButton;
@@ -79,9 +80,14 @@ namespace Purpose
 
         private Texture2D pauseScreen;
         private GameObject returnToGameButton;
+
+        private Texture2D nextWaveScreen;
+        private GameObject goOnButton;
         private GameObject upgradesButton;
 
         public Texture2D gameOver;
+        public GameObject returnToMenuButton;
+
         //Field for the Reader
         private Reader reader;
 
@@ -179,18 +185,16 @@ namespace Purpose
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // Load in textures
-            //temporary textures
             tempTexture = Content.Load<Texture2D>("pineapple");
-            //tempCrouchTexture = Content.Load<Texture2D>("smallerPineapple(1)");
-            trent = Content.Load<Texture2D>("trent");
             comicSans24 = Content.Load<SpriteFont>("ComicSans24");
             agency30 = Content.Load<SpriteFont>("Agency30");
-            startScreen = Content.Load<Texture2D>("metalBackground2");
+            startScreen = Content.Load<Texture2D>("StartMenu");
             buttonFrame = Content.Load<Texture2D>("buttonFrame2");
             roundedFrame = Content.Load<Texture2D>("roundedFrame");
             upgradeScreen = Content.Load<Texture2D>("UpgradeUI");
-            pauseScreen = Content.Load<Texture2D>("pauseMenu");
-            gameOver = Content.Load<Texture2D>("gameOver");
+            pauseScreen = Content.Load<Texture2D>("PauseMenu");
+            nextWaveScreen = Content.Load<Texture2D>("NextWaveMenu");
+            gameOver = Content.Load<Texture2D>("GameOver");
 
             //temporary background
             whiteBack = Content.Load<Texture2D>("whiteback");
@@ -201,10 +205,12 @@ namespace Purpose
             basePlatform = Content.Load<Texture2D>("PlatformTest");
             notBasePlatform = Content.Load<Texture2D>("PlatformTest2");
 
-            startButton = new GameObject(buttonFrame, new Rectangle(500, 365, 300, 100));
-            returnToPauseButton = new GameObject(buttonFrame, new Rectangle(25,340,170,110));
-            returnToGameButton = new GameObject(buttonFrame, new Rectangle(530, 280, 280, 160));
-            upgradesButton = new GameObject(buttonFrame, new Rectangle(530, 495, 280, 100));
+            startButton = new GameObject(buttonFrame, new Rectangle(500, 335, 349, 155));
+            returnToNewWaveButton = new GameObject(buttonFrame, new Rectangle(10,340,242,109));
+            returnToGameButton = new GameObject(buttonFrame, new Rectangle(500, 280, 349, 160));
+            goOnButton = new GameObject(buttonFrame, new Rectangle(500, 272, 349, 160));
+            upgradesButton = new GameObject(buttonFrame, new Rectangle(500, 512, 349, 160));
+            returnToMenuButton = new GameObject(buttonFrame, new Rectangle(500, 343, 349, 160));
 
             groundPoundButton = new GameObject(roundedFrame, new Rectangle(337,200,118,118));
             attackUpButton = new GameObject(roundedFrame, new Rectangle(510, 292, 118, 118));
@@ -413,15 +419,6 @@ namespace Purpose
                     {
                         spriteBatch.Draw(returnToGameButton.Texture, returnToGameButton.Position, Color.White);
                     }
-
-                    if (upgradesButton.Intersects(ms.Position))
-                    {
-                        spriteBatch.Draw(upgradesButton.Texture, upgradesButton.Position, Color.Black);
-                    }
-                    else
-                    {
-                        spriteBatch.Draw(upgradesButton.Texture, upgradesButton.Position, Color.White);
-                    }
                     break;
 
                 case GameState.UpgradeMenu:
@@ -429,13 +426,13 @@ namespace Purpose
                     spriteBatch.DrawString(agency30, "Upgrade Points: " + gameManager.Player.UgManager.UpgradePoints.ToString(), new Vector2(1090,730), Color.White);
                     //spriteBatch.Draw(buttonFrame, new Rectangle(1192, 722, 54, 60), Color.Black);
 
-                    if (returnToPauseButton.Intersects(ms.Position))
+                    if (returnToNewWaveButton.Intersects(ms.Position))
                     {
-                        spriteBatch.Draw(returnToPauseButton.Texture, returnToPauseButton.Position, Color.Black);
+                        spriteBatch.Draw(returnToNewWaveButton.Texture, returnToNewWaveButton.Position, Color.Black);
                     }
                     else
                     {
-                        spriteBatch.Draw(returnToPauseButton.Texture, returnToPauseButton.Position, Color.White);
+                        spriteBatch.Draw(returnToNewWaveButton.Texture, returnToNewWaveButton.Position, Color.White);
                     }
 
                     if (groundPoundButton.Intersects(ms.Position))
@@ -493,6 +490,29 @@ namespace Purpose
                     }
                     break;
 
+                case GameState.NextWave:
+                    spriteBatch.Draw(nextWaveScreen, new Rectangle(0, 0, GraphicsDevice.Viewport.Width, GraphicsDevice.Viewport.Height), Color.White);
+
+                    if (goOnButton.Intersects(ms.Position))
+                    {
+                        spriteBatch.Draw(goOnButton.Texture, goOnButton.Position, Color.Black);
+                    }
+                    else
+                    {
+                        spriteBatch.Draw(goOnButton.Texture, goOnButton.Position, Color.White);
+                    }
+
+
+                    if (upgradesButton.Intersects(ms.Position))
+                    {
+                        spriteBatch.Draw(upgradesButton.Texture, upgradesButton.Position, Color.Black);
+                    }
+                    else
+                    {
+                        spriteBatch.Draw(upgradesButton.Texture, upgradesButton.Position, Color.White);
+                    }
+                    break;
+
                 case GameState.GameOver:
                     // Temp drawing stuffs
                     //spriteBatch.Draw(background, new Vector2(0, 0), Color.White);
@@ -500,6 +520,14 @@ namespace Purpose
                     //    Color.Yellow);
 
                     spriteBatch.Draw(gameOver, new Rectangle(0, 0, GraphicsDevice.Viewport.Width, GraphicsDevice.Viewport.Height), Color.White);
+                    if (returnToMenuButton.Intersects(ms.Position))
+                    {
+                        spriteBatch.Draw(returnToMenuButton.Texture, returnToMenuButton.Position, Color.Black);
+                    }
+                    else
+                    {
+                        spriteBatch.Draw(returnToMenuButton.Texture, returnToMenuButton.Position, Color.White);
+                    }
 
                     break;
             }
@@ -576,22 +604,22 @@ namespace Purpose
                         if (gameManager.WaveNumber == WaveNumber.One)
                         {
                             gameManager.WaveNumber = WaveNumber.Two;
-                            gameManager.GameState = GameState.Menu;
+                            gameManager.GameState = GameState.NextWave;
                         }
                         else if (gameManager.WaveNumber == WaveNumber.Two)
                         {
                             gameManager.WaveNumber = WaveNumber.Three;
-                            gameManager.GameState = GameState.Menu;
+                            gameManager.GameState = GameState.NextWave;
                         }
                         else if (gameManager.WaveNumber == WaveNumber.Three)
                         {
                             gameManager.WaveNumber = WaveNumber.Four;
-                            gameManager.GameState = GameState.Menu;
+                            gameManager.GameState = GameState.NextWave;
                         }
                         else if (gameManager.WaveNumber == WaveNumber.Four)
                         {
                             gameManager.WaveNumber = WaveNumber.Five;
-                            gameManager.GameState = GameState.Menu;
+                            gameManager.GameState = GameState.NextWave;
                         }
                         else if (gameManager.WaveNumber == WaveNumber.Five)
                         {
@@ -619,9 +647,9 @@ namespace Purpose
                     break;
 
                 case GameState.UpgradeMenu:
-                    if (returnToPauseButton.Intersects(ms.Position) && ms.LeftButton == ButtonState.Pressed)
+                    if (returnToNewWaveButton.Intersects(ms.Position) && ms.LeftButton == ButtonState.Pressed)
                     {
-                        gameManager.GameState = GameState.Pause;
+                        gameManager.GameState = GameState.NextWave;
                     }
 
                     if (groundPoundButton.Intersects(ms.Position) && ms.LeftButton == ButtonState.Pressed && previousMs.LeftButton == ButtonState.Released)
@@ -648,7 +676,22 @@ namespace Purpose
                     {
                         gameManager.Player.DashDistance = gameManager.Player.UgManager.DashDistanceUpgrade(gameManager.Player.DashDistance);
                     }
+                    break;
 
+                case GameState.NextWave:
+                    camera.Zoom = 1.0f;
+                    camera.Position = new Vector2(0, 0);
+
+                    gameManager.ResetGame(camera, rng, worldLeftEndWidth, worldRightEndWidth, gameTime, tempTexture, waveNumber);
+
+                    if (goOnButton.Intersects(ms.Position) && ms.LeftButton == ButtonState.Pressed && previousMs.LeftButton == ButtonState.Released)
+                    {
+                        gameManager.GameState = GameState.Game;
+                    }
+                    else if (upgradesButton.Intersects(ms.Position) && ms.LeftButton == ButtonState.Pressed && previousMs.LeftButton == ButtonState.Released)
+                    {
+                        gameManager.GameState = GameState.UpgradeMenu;
+                    }
                     break;
 
                 case GameState.GameOver:
@@ -656,7 +699,7 @@ namespace Purpose
                     camera.Position = new Vector2(0, 0);
                     gameManager.WaveNumber = WaveNumber.One;
                     // Press enter to go back to menu
-                    if (kbState.IsKeyDown(Keys.Enter))
+                    if (returnToMenuButton.Intersects(ms.Position) && ms.LeftButton == ButtonState.Pressed && previousMs.LeftButton == ButtonState.Released)
                     {
                         gameManager.GameState = GameState.Menu;
                     }
