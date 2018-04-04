a2233ceb410600204aa875ffcb10102a5a8d44a3
The End Is Nigh <app2076@rit.edu>
2018-04-04 19:04:25 -0400

    Took away onBasePlatform method Limited player attack speed

--- Purpose/Character.cs
+++ Purpose/Character.cs
@@ -37,10 +37,8 @@ namespace Purpose
         public Character(Texture2D texture) : base(texture) { }
 
         //methods
-        public abstract int Attack(Rectangle rectangle, GameTime gameTime);
+        public abstract int Attack(Character character, GameTime gameTime);
 
         public abstract void TakeDamage(int damage);
-
-        public abstract bool OnBasePlatform(Rectangle characterToBeChecked);
     }
 }
